2bf0a4ee70e341ae5120f126730e09f06d9dbf09
David Halewood <davidnhalewood@hotmail.com>
2018-07-04 21:03:24 +0100

    Make http post request work

--- app/routes/post_routes.js
+++ app/routes/post_routes.js
@@ -1,6 +1,14 @@
 module.exports = function (app, db) {
   app.post('/posts', (req, res) => {
-    // Post creation here
-    res.send('Hello World, from the post route.')
+    console.log(req.body)
+    const post = { title: req.body.title, upvotes: 0, views: 0}
+    db.collection('posts').insert(post, (err, result) => {
+      if (err) { 
+        console.log(err)
+        res.send({ 'error': 'An error has occurred' })
+      } else {
+        res.send(result.ops[0])
+      }
+    })
   })
 }
