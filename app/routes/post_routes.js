c991cdae86f0555487851bf7e07deb734ab53a71
David Halewood <davidnhalewood@hotmail.com>
2018-07-05 23:10:39 +0100

    Add get post by id route

--- app/routes/post_routes.js
+++ app/routes/post_routes.js
@@ -1,4 +1,18 @@
+const ObjectID = require('mongodb').ObjectID
+
 module.exports = function (app, db) {
+  app.get('/posts/:id', (req, res) => {
+    const id = '5b3e95860283400014de321f'
+    const details = { 'title': new ObjectID(id) };
+    db.collection('posts').findOne(details, (err, item) => {
+      if (err) {
+        res.send({'error':'An error has occurred'});
+      } else {
+        res.send(item);
+      }
+    })
+  })
+
   app.post('/posts', (req, res) => {
     console.log(req.body)
     const post = { title: req.body.title, upvotes: 0, views: 0}
