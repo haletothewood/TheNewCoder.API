5286016183761980af6eb56c62e4b75fe191f315
David Halewood <davidnhalewood@hotmail.com>
2018-07-05 22:22:16 +0100

    Fix connection string

--- config/db.js
+++ config/db.js
@@ -1,3 +1,3 @@
 module.exports = {
-  url: `mongodb://${process.env.user}:${process.env.password}.mlab.com:19651/the-new-coder`
+  url: `mongodb://${process.env.user}:${process.env.password}@ds119651.mlab.com:19651/the-new-coder`
 };
